b59b44846e2cd2a1d7205bdeba50234b41129884
mayankbishtt <mayankbisht1947@gmail.com>
2026-07-29 09:04:49 +0530

    Add reusable ALB module

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -13,3 +13,13 @@ module "network" {
   private_subnet_2 = var.private_subnet_2
 
 }
+
+module "alb" {
+
+  source = "../../modules/alb"
+
+  vpc_id = module.network.vpc_id
+
+  public_subnet_ids = module.network.public_subnet_ids
+
+}
--- infra/envs/dev/outputs.tf
+++ infra/envs/dev/outputs.tf
@@ -3,3 +3,9 @@ output "vpc_id" {
   value = module.network.vpc_id
 
 }
+
+output "alb_dns_name" {
+
+  value = module.alb.alb_dns_name
+
+}
