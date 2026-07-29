72056d36c75e1059bb302ff5952f74f171f94aa7
mayankbishtt <mayankbisht1947@gmail.com>
2026-07-29 09:10:10 +0530

    Add ECS Fargate module

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -23,3 +23,17 @@ module "alb" {
   public_subnet_ids = module.network.public_subnet_ids
 
 }
+
+module "ecs" {
+
+  source = "../../modules/ecs"
+
+  vpc_id = module.network.vpc_id
+
+  private_subnet_ids = module.network.private_subnet_ids
+
+  target_group_arn = module.alb.target_group_arn
+
+  alb_security_group_id = module.alb.alb_security_group_id
+
+}
